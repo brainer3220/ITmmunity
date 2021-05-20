52046d191760569bc144cce99e37108cb24dab6a
brainer <brainer@kakao.com>
2021-05-21 02:52:08 +0900

    Add WearOS MainList

--- ITmmunity/src/main/java/com/brainer/itmmunity/MainActivity.kt
+++ ITmmunity/src/main/java/com/brainer/itmmunity/MainActivity.kt
@@ -2,6 +2,7 @@ package com.brainer.itmmunity
 
 import android.app.Activity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 import com.brainer.itmmunity.databinding.ActivityMainBinding
 
 class MainActivity : Activity() {
@@ -14,5 +15,23 @@ class MainActivity : Activity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val dummies = listOf(
+            Content(
+                title = "삼성전자, 갤럭시 엑스커버 프로 원 UI 3.0 업데이트 배포",
+                numComment = 0,
+                hit = 784,
+                image = "underkg.co.kr",
+                url = "underkg.co.kr"
+            ),
+            Content(
+                title = "삼성전자, 갤럭시 엑스커버 프로 원 UI 3.0 업데이트 배포",
+                numComment = 0,
+                hit = 784,
+                image = "underkg.co.kr",
+                url = "underkg.co.kr"
+            )
+        )
+
+//        main_list = findViewById<RecyclerView>(R.id.main_list)
     }
 }
