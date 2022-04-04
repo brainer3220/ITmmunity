8941e245c9e334723003cf6e0bd12d9643206cf1
brainer <brainer@kakao.com>
2022-04-04 18:42:15 +0900

    Change getConnectState logic

--- app/src/main/java/com/brainer/itmmunity/ViewModel/BackGroundViewModel.kt
+++ app/src/main/java/com/brainer/itmmunity/ViewModel/BackGroundViewModel.kt
@@ -5,7 +5,6 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.brainer.itmmunity.Croll.Croll
 import com.brainer.itmmunity.Utility.NetworkConnectCheck
 import kotlinx.coroutines.*
 
@@ -21,24 +20,23 @@ class BackGroundViewModel(context: Context) : ViewModel() {
 
     @OptIn(DelicateCoroutinesApi::class)
     fun getConnectState() {
-        GlobalScope.launch {
-            CoroutineScope(Dispatchers.Default).launch {
-                while(true) {
-                    delay(3000L)
-                    kotlin.runCatching {
-                        NetworkConnectCheck().isConnectNetwork(_context.value!!)
-                    }.onSuccess {
-                        if (isConnect.value != it) {
-                            CoroutineScope(Dispatchers.Main).launch {
-                                _isConnect.value = it
-                            }
-                            CoroutineScope(Dispatchers.Default).launch {
-                                Log.d("isConnection", it.toString())
-                            }
+        CoroutineScope(Dispatchers.Default).launch {
+            while (true) {
+                delay(3000L)
+                kotlin.runCatching {
+                    NetworkConnectCheck().isConnectNetwork(_context.value!!)
+                }.onSuccess {
+                    if (isConnect.value != it) {
+                        CoroutineScope(Dispatchers.Main).launch {
+                            _isConnect.value = it
+                        }
+                        CoroutineScope(Dispatchers.Default).launch {
+                            Log.d("isConnection", it.toString())
                         }
                     }
                 }
             }
         }
+
     }
 }
