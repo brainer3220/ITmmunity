fc07a54a991a1d4790cf03b785ef679403b44dc8
brainer <brainer@kakao.com>
2022-04-08 17:23:56 +0900

    Change Image view for MainView

--- app/src/main/java/com/brainer/itmmunity/Componant/MainViewComponant.kt
+++ app/src/main/java/com/brainer/itmmunity/Componant/MainViewComponant.kt
@@ -3,7 +3,6 @@ package com.brainer.itmmunity.Componant
 import android.annotation.SuppressLint
 import android.util.Log
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.isSystemInDarkTheme
@@ -12,19 +11,26 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.*
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.brainer.itmmunity.Croll.Croll
 import com.brainer.itmmunity.R
 import com.brainer.itmmunity.ViewModel.MainViewModel
-import com.google.accompanist.glide.rememberGlidePainter
+import com.facebook.shimmer.Shimmer
+import com.skydoves.landscapist.CircularReveal
+import com.skydoves.landscapist.ShimmerParams
+import com.skydoves.landscapist.glide.GlideImage
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.Dispatchers
@@ -155,15 +161,17 @@ fun NewsListOf(aNews: Croll.Content, mainViewModel: MainViewModel, modifier: Mod
                 if (aNews.image != null) {
                     Log.i("Thumbnail", "Thumbnail load success")
                     Log.d("Thumbnail", "Thumbnail: " + aNews.image)
-                    Image(
-                        modifier = Modifier
-                            .weight(1f)
-                            .clip(RoundedCornerShape(8.dp)),
-                        painter = rememberGlidePainter(
-                            request = aNews.image,
-                        ),
-                        contentDescription = stringResource(R.string.main_thumbnail),
-                    )
+                    Box(modifier = Modifier
+                        .weight(1f)
+                        .clip(RoundedCornerShape(8.dp))) {
+                        GlideImage(
+                            modifier = Modifier.fillMaxSize(),
+                            imageModel = aNews.image,
+                            contentScale = ContentScale.FillWidth,
+                            contentDescription = stringResource(id = R.string.main_thumbnail),
+                            circularReveal = CircularReveal(duration = 350),
+                        )
+                    }
                     Spacer(modifier = Modifier.padding(5.dp))
                 }
                 Text(
