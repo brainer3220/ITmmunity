3af742d4271398e8e65f711bc41ec6f55bf5a470
brainer <brainer@kakao.com>
2022-04-29 14:42:44 +0900

    Underkg URL to consider defining

--- ITmmunity/src/main/java/com/brainer/itmmunity/data.kt
+++ ITmmunity/src/main/java/com/brainer/itmmunity/data.kt
@@ -14,14 +14,14 @@ val dummies = listOf(
         title = "삼성전자, 갤럭시 엑스커버 프로 원 UI 3.0 업데이트 배포",
         numComment = 0,
         hit = 784,
-        image = "underkg.co.kr",
-        url = "underkg.co.kr"
+        image = UNDERKG_URL,
+        url = UNDERKG_URL
     ),
     Content(
         title = "삼성전자, 갤럭시 엑스커버 프로 원 UI 3.0 업데이트 배포",
         numComment = 0,
         hit = 784,
-        image = "underkg.co.kr",
-        url = "underkg.co.kr"
+        image = UNDERKG_URL,
+        url = UNDERKG_URL
     )
 )
