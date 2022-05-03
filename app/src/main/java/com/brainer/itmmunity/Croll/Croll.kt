917b4bd421a78d4c9925fae327f395bb6648c3d5
brainer <brainer@kakao.com>
2022-05-03 19:48:13 +0900

    Underkg url change
    Add 'r'

--- app/src/main/java/com/brainer/itmmunity/Croll/Croll.kt
+++ app/src/main/java/com/brainer/itmmunity/Croll/Croll.kt
@@ -8,7 +8,7 @@ import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import org.jsoup.select.Elements
 
-const val UNDERKG_URL = "undekg.co.kr"
+const val UNDERKG_URL = "underkg.co.kr"
 
 open class Croll {
     @Parcelize
