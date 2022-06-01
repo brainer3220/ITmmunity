c875edacdcbf15c0bae45b0f328c22fbec59f2e6
brainer <brainer@kakao.com>
2022-06-01 20:10:08 +0900

    Split file for ComposableTransition

--- app/src/main/java/com/brainer/itmmunity/navcontrol/NavGraph.kt
+++ app/src/main/java/com/brainer/itmmunity/navcontrol/NavGraph.kt
@@ -1,86 +1,17 @@
 package com.brainer.itmmunity.navcontrol
 
-import androidx.compose.animation.*
-import androidx.compose.animation.core.tween
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.runtime.Composable
-import androidx.navigation.*
-import com.brainer.itmmunity.*
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.navArgument
+import com.brainer.itmmunity.ContentView
+import com.brainer.itmmunity.MainView
 import com.brainer.itmmunity.viewmodel.BackGroundViewModel
 import com.brainer.itmmunity.viewmodel.MainViewModel
 import com.google.accompanist.navigation.animation.AnimatedNavHost
 import com.google.accompanist.navigation.animation.composable
-
-
-@ExperimentalAnimationApi
-sealed class ComposableTransition(
-    val enterTransition: (AnimatedContentScope<NavBackStackEntry>.() -> EnterTransition),
-    val exitTransition: (AnimatedContentScope<NavBackStackEntry>.() -> ExitTransition?)?,
-    val popEnterTransition: (AnimatedContentScope<NavBackStackEntry>.() -> EnterTransition?)? = enterTransition,
-    val popExitTransition: (AnimatedContentScope<NavBackStackEntry>.() -> ExitTransition?)? = exitTransition
-) {
-    object Main : ComposableTransition(
-        enterTransition = {
-            when (initialState.destination.route) {
-                "Red" ->
-                    slideIntoContainer(
-                        AnimatedContentScope.SlideDirection.Left,
-                        animationSpec = tween(ANIMATION_DURATION)
-                    )
-                else -> {
-                    slideIntoContainer(
-                        AnimatedContentScope.SlideDirection.Left,
-                        animationSpec = tween(ANIMATION_DURATION)
-                    )
-                }
-            }
-        },
-        exitTransition = {
-            when (targetState.destination.route) {
-                "Blue" ->
-                    slideOutOfContainer(
-                        AnimatedContentScope.SlideDirection.Left,
-                        animationSpec = tween(ANIMATION_DURATION)
-                    )
-                else -> null
-            }
-        },
-        popEnterTransition = {
-            when (initialState.destination.route) {
-                "Blue" ->
-                    slideIntoContainer(
-                        AnimatedContentScope.SlideDirection.Right,
-                        animationSpec = tween(ANIMATION_DURATION)
-                    )
-                else -> null
-            }
-        },
-        popExitTransition = {
-            when (targetState.destination.route) {
-                "Blue" ->
-                    slideOutOfContainer(
-                        AnimatedContentScope.SlideDirection.Right,
-                        animationSpec = tween(ANIMATION_DURATION)
-                    )
-                else -> null
-            }
-        }
-    )
-
-    object ContentView : ComposableTransition(
-        enterTransition = {
-            slideInVertically(initialOffsetY = { ANIMATION_INIT_OFFSET_Y }) + fadeIn()
-        },
-        exitTransition = {
-            slideOutVertically(targetOffsetY = { ANIMATION_TARGET_OFFSET_Y }) + fadeOut()
-        },
-        popEnterTransition = {
-            slideInVertically(initialOffsetY = { ANIMATION_INIT_OFFSET_Y }) + fadeIn()
-        },
-        popExitTransition = {
-            slideOutVertically(targetOffsetY = { ANIMATION_TARGET_OFFSET_Y }) + fadeOut()
-        }
-    )
-}
 
 @ExperimentalAnimationApi
 @Composable
