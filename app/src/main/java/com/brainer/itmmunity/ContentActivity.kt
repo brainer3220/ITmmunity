830d6665bf7d7daab9110b1f8976d8f02b12b883
brainer <brainer@kakao.com>
2022-07-15 00:30:26 +0900

    Add AdMob view for ContentView

--- app/src/main/java/com/brainer/itmmunity/ContentActivity.kt
+++ app/src/main/java/com/brainer/itmmunity/ContentActivity.kt
@@ -5,12 +5,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import com.brainer.itmmunity.Croll.Croll
+import com.brainer.itmmunity.componant.AdMobCompose
 import com.brainer.itmmunity.componant.AppBar
 import com.brainer.itmmunity.ui.theme.ITmmunity_AndroidTheme
 
@@ -31,7 +36,16 @@ class ContentActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     AppBar(content = content, context = LocalContext.current) {
-                        ContentView(content)
+                        Column {
+                            ContentView(content)
+
+                            AdMobCompose(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .height(250.dp),
+                                adId = "ca-app-pub-1000428004132415/8571464204"
+                            )
+                        }
                     }
                 }
             }
