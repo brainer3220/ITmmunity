3a395217a4396a93cfd9c20d1a282bfe48e7dbd9
brainer <brainer@kakao.com>
2022-08-05 17:51:47 +0900

    Add PreferencePreview Compose

---
+++ app/src/main/java/com/brainer/itmmunity/preference/Preference.kt
@@ -0,0 +1,17 @@
+package com.brainer.itmmunity.preference
+
+import android.widget.Toast
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import com.jamal.composeprefs3.ui.PrefsScreen
+import com.jamal.composeprefs3.ui.prefs.TextPref
+
+@Preview
+@Composable
+fun PreferencePreview() {
+
+}
