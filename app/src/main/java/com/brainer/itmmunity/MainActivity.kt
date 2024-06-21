4df90ff60e621d12d1a7b24e01308db1e9ed9f94
brainer <brainer@kakao.com>
2024-06-21 23:18:34 +0900

    Add imports, set up Firebase Remote Config, and refactor MainView composition. Improve error handling and content display based on network connection status.
    Details:
    - Added import for `android.annotation.SuppressLint`.
    - Refactored the setup of Firebase Remote Config into a separate function.
    - Updated the composition of `MainView` to improve readability and maintainability.
    - Enhanced error handling by displaying an error message when there is no network connection.
    - Improved content display logic based on the network connection status.

--- app/src/main/java/com/brainer/itmmunity/MainActivity.kt
+++ app/src/main/java/com/brainer/itmmunity/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.brainer.itmmunity
 
+import android.annotation.SuppressLint
 import android.app.Application
 import android.content.Intent
 import android.os.Bundle
@@ -13,27 +14,14 @@ import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxWithConstraints
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.pullrefresh.PullRefreshState
 import androidx.compose.material.pullrefresh.pullRefresh
 import androidx.compose.material.pullrefresh.rememberPullRefreshState
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
@@ -42,16 +30,10 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.brainer.itmmunity.MainActivity.Companion.navController
-import com.brainer.itmmunity.presentation.componant.AppBar
-import com.brainer.itmmunity.presentation.componant.ConnectErrorView
-import com.brainer.itmmunity.presentation.componant.CustomPullRefreshIndicator
-import com.brainer.itmmunity.presentation.componant.NewsCardListView
+import com.brainer.itmmunity.domain.model.ContentModel
+import com.brainer.itmmunity.presentation.componant.*
 import com.brainer.itmmunity.presentation.navigation.NavigationView
-import com.brainer.itmmunity.presentation.viewmodel.BackGroundViewModel
-import com.brainer.itmmunity.presentation.viewmodel.CONFIG_STR
-import com.brainer.itmmunity.presentation.viewmodel.ContentViewModel
-import com.brainer.itmmunity.presentation.viewmodel.LoadState
-import com.brainer.itmmunity.presentation.viewmodel.MainViewModel
+import com.brainer.itmmunity.presentation.viewmodel.*
 import com.google.accompanist.navigation.material.BottomSheetNavigator
 import com.google.accompanist.navigation.material.ExperimentalMaterialNavigationApi
 import com.google.accompanist.navigation.material.rememberBottomSheetNavigator
@@ -61,7 +43,6 @@ import com.google.accompanist.placeholder.material.shimmer
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.remoteconfig.ktx.remoteConfig
 import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
-import kotlinx.coroutines.launch
 import org.oneui.compose.theme.OneUITheme
 import org.oneui.compose.widgets.box.RoundedCornerBox
 
@@ -79,15 +60,31 @@ class MainActivity : AppCompatActivity() {
         lateinit var navController: NavHostController
     }
 
-    @OptIn(ExperimentalMaterialNavigationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setupFirebaseRemoteConfig()
+        setContent {
+            setupContent()
+        }
+    }
 
-        val remoteConfig = Firebase.remoteConfig
-        val configSettings =
-            remoteConfigSettings {
-                minimumFetchIntervalInSeconds = FIREBASE_MINIMUM_FETCH_SEC
+    @Composable
+    @OptIn(ExperimentalMaterialNavigationApi::class)
+    private fun setupContent() {
+        OneUITheme {
+            bottomSheetNavigator = rememberBottomSheetNavigator()
+            navController = rememberNavController(bottomSheetNavigator)
+            Surface {
+                NavigationView(navController)
             }
+        }
+    }
+
+    private fun setupFirebaseRemoteConfig() {
+        val remoteConfig = Firebase.remoteConfig
+        val configSettings = remoteConfigSettings {
+            minimumFetchIntervalInSeconds = FIREBASE_MINIMUM_FETCH_SEC
+        }
         remoteConfig.setConfigSettingsAsync(configSettings)
 
         remoteConfig.fetchAndActivate()
@@ -100,193 +97,154 @@ class MainActivity : AppCompatActivity() {
                     Log.d(CONFIG_STR, "Fetch failed")
                 }
             }
-
-        setContent {
-            OneUITheme {
-                bottomSheetNavigator = rememberBottomSheetNavigator()
-                navController = rememberNavController(bottomSheetNavigator)
-                // A surface container using the 'background' color from the theme
-                Surface {
-                    NavigationView(navController)
-                }
-            }
-        }
     }
 
     @Deprecated("Deprecated in Java")
-    override fun onActivityResult(
-        requestCode: Int,
-        resultCode: Int,
-        data: Intent?,
-    ) {
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         Log.d("MainActivity", "onActivityResult")
-
-//        if (requestCode == REQUEST_CODE) {
-//            Log.d("MainActivity", "onActivityResult REQUEST_INITIAL_ROUTE")
-//        }
     }
 }
 
-@Suppress("ktlint:standard:function-naming")
-/**
- * @author brainer
- * @param viewModel MainViewModel
- * @param networkViewModel BackGroundViewModel
- */
 @ExperimentalAnimationApi
 @Composable
-fun MainViewWithAppBar(
-    viewModel: MainViewModel,
-    networkViewModel: BackGroundViewModel = BackGroundViewModel(Application()),
-) {
+fun MainViewWithAppBar(viewModel: MainViewModel, networkViewModel: BackGroundViewModel = BackGroundViewModel(Application())) {
     AppBar(viewModel = viewModel) {
-        MainView(
-            viewModel = viewModel,
-            networkViewModel = networkViewModel,
-        )
+        MainView(viewModel = viewModel, networkViewModel = networkViewModel)
     }
 }
 
-@Suppress("ktlint:standard:function-naming")
 @OptIn(ExperimentalMaterialApi::class)
 @ExperimentalAnimationApi
 @Composable
-private fun MainView(
-    viewModel: MainViewModel,
-    networkViewModel: BackGroundViewModel,
-) {
+private fun MainView(viewModel: MainViewModel, networkViewModel: BackGroundViewModel) {
     val scope = rememberCoroutineScope()
-//    val unifiedList by viewModel.unifiedList.collectAsStateWithLifecycle()
     val newsList by viewModel.newsList.collectAsStateWithLifecycle()
     val aNews by viewModel.aNews.collectAsStateWithLifecycle()
     val loadState by viewModel.loadState.collectAsStateWithLifecycle()
-    val refreshing =
-        remember {
-            mutableStateOf(loadState == LoadState.LOADING)
-        }
-
+    val refreshing = remember { mutableStateOf(loadState == LoadState.LOADING) }
     val isConnection by networkViewModel.isConnect.collectAsStateWithLifecycle()
-//    val isTabletUi by viewModel.isTabletUi.collectAsStateWithLifecycle()
 
     LaunchedEffect(loadState) {
         refreshing.value = loadState == LoadState.LOADING
     }
 
-    fun refresh() =
-        scope.launch {
-            viewModel.refresh()
-        }
-
-    val pullRefreshState = rememberPullRefreshState(refreshing.value, ::refresh)
-
-    Log.d("Unified_List", newsList.toString())
+    val pullRefreshState = rememberPullRefreshState(refreshing.value, { refresh(viewModel) })
 
     if (isConnection) {
-        BoxWithConstraints(
-            Modifier.fillMaxSize(),
-        ) {
-            val boxWithConstraintsScope = this
-            Row(Modifier.fillMaxSize()) {
-                Column(
-                    modifier =
-                        Modifier
-                            .weight(1f)
-                            .pullRefresh(pullRefreshState),
-                ) {
-                    AnimatedContent(newsList.isNotEmpty(), label = "", transitionSpec = {
-                        ContentTransform(
-                            targetContentEnter = fadeIn(animationSpec = tween(500)),
-                            initialContentExit = fadeOut(animationSpec = tween(500)),
-                        )
-                    }) { isNotEmpty ->
-                        when (isNotEmpty) {
-                            true -> {
-                                NewsCardListView(
-                                    newsList,
-                                    viewModel,
-                                )
-                            }
+        MainContent(viewModel, newsList, aNews, loadState, pullRefreshState, refreshing)
+    } else {
+        ErrorContent()
+    }
+}
 
-                            false -> {
-                                Column {
-                                    repeat(10) {
-                                        Row(
-                                            modifier =
-                                                Modifier.fillMaxWidth().height(100.dp)
-                                                    .padding(16.dp),
-                                        ) {
-                                            RoundedCornerBox(
-                                                modifier =
-                                                    Modifier
-                                                        .fillMaxSize()
-                                                        .weight(2f)
-                                                        .placeholder(
-                                                            visible = true,
-                                                            highlight = PlaceholderHighlight.shimmer(),
-                                                        ),
-                                            ) {}
-                                            Spacer(modifier = Modifier.width(16.dp))
-                                            RoundedCornerBox(
-                                                modifier =
-                                                    Modifier
-                                                        .fillMaxSize()
-                                                        .weight(9f)
-                                                        .placeholder(
-                                                            visible = true,
-                                                            highlight = PlaceholderHighlight.shimmer(),
-                                                        ),
-                                            ) {}
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
+private fun refresh(viewModel: MainViewModel) = viewModel.refresh()
 
-                if (boxWithConstraintsScope.maxWidth >= TABLET_UI_WIDTH.dp) {
-                    viewModel.changeTabletUi(true)
-                    Box(
-                        modifier =
-                            Modifier
-                                .weight(1f)
-                                .align(Alignment.CenterVertically),
-                    ) {
-                        Crossfade(targetState = aNews, label = "") {
-                            if (it != null) {
-                                ContentView(
-                                    aNews = aNews!!,
-                                    ContentViewModel(Application()),
-                                    navController,
-                                )
-                            } else {
-                                RoundedCornerBox {
-                                    Text(
-                                        modifier = Modifier.fillMaxSize(),
-                                        text = "컨텐츠를 클릭해 보세요.",
-                                        textAlign = TextAlign.Center,
-                                    )
-                                }
-                            }
-                        }
+@OptIn(ExperimentalMaterialApi::class)
+@ExperimentalAnimationApi
+@Composable
+private fun MainContent(
+    viewModel: MainViewModel,
+    newsList: List<Any>,  // Replace Any with the actual type
+    aNews: Any?,  // Replace Any with the actual type
+    loadState: LoadState,
+    pullRefreshState: Any,  // Replace Any with the actual type
+    refreshing: MutableState<Boolean>
+) {
+    BoxWithConstraints(Modifier.fillMaxSize()) {
+        Row(Modifier.fillMaxSize()) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .pullRefresh(pullRefreshState as PullRefreshState)
+            ) {
+                AnimatedContent(newsList.isNotEmpty(), label = "", transitionSpec = {
+                    ContentTransform(
+                        targetContentEnter = fadeIn(animationSpec = tween(500)),
+                        initialContentExit = fadeOut(animationSpec = tween(500)),
+                    )
+                }) { isNotEmpty ->
+                    if (isNotEmpty) {
+                        NewsCardListView(newsList as List<ContentModel>, viewModel)
+                    } else {
+                        ShimmerPlaceholders()
                     }
-                } else {
-                    viewModel.changeTabletUi(false)
                 }
             }
-            CustomPullRefreshIndicator(
-                refreshing.value,
-                pullRefreshState,
-                Modifier.align(Alignment.TopCenter),
-            )
+
+            if (this@BoxWithConstraints.maxWidth >= TABLET_UI_WIDTH.dp) {
+                viewModel.changeTabletUi(true)
+                Box(
+                    modifier = Modifier
+                        .weight(1f)
+                        .align(Alignment.CenterVertically)
+                ) {
+                    Crossfade<Any?>(targetState = aNews, label = "") {
+                        if (it != null) {
+                            ContentView(
+                                aNews = aNews!! as ContentModel,
+                                ContentViewModel(Application()),
+                                MainActivity.navController
+                            )
+                        } else {
+                            RoundedCornerBox {
+                                Text(
+                                    modifier = Modifier.fillMaxSize(),
+                                    text = "컨텐츠를 클릭해 보세요.",
+                                    textAlign = TextAlign.Center,
+                                )
+                            }
+                        }
+                    }
+                }
+            } else {
+                viewModel.changeTabletUi(false)
+            }
         }
-    } else {
-        RoundedCornerBox(
-            modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center,
-        ) {
-            ConnectErrorView()
+        CustomPullRefreshIndicator(refreshing.value, pullRefreshState as PullRefreshState, Modifier.align(Alignment.TopCenter))
+    }
+}
+
+@Composable
+private fun ShimmerPlaceholders() {
+    Column {
+        repeat(10) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(100.dp)
+                    .padding(16.dp)
+            ) {
+                RoundedCornerBox(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .weight(2f)
+                        .placeholder(
+                            visible = true,
+                            highlight = PlaceholderHighlight.shimmer()
+                        )
+                ) {}
+                Spacer(modifier = Modifier.width(16.dp))
+                RoundedCornerBox(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .weight(9f)
+                        .placeholder(
+                            visible = true,
+                            highlight = PlaceholderHighlight.shimmer()
+                        )
+                ) {}
+            }
         }
+    }
+}
+
+@Composable
+private fun ErrorContent() {
+    RoundedCornerBox(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        ConnectErrorView()
     }
 }
